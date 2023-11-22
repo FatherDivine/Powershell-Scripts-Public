b373d8f1bc1b2afa2e93fd67af0285bbd55a26f4
FatherDivine <150489354+FatherDivine@users.noreply.github.com>
2023-11-22 13:21:42 -0700

    Add files via upload

--- MS Windows Online Updates/MSWOU.ps1
+++ MS Windows Online Updates/MSWOU.ps1
@@ -26,6 +26,12 @@
   <Example goes here. Repeat this attribute for more than one example>
 #>
 
+if (-NOT ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator"))  
+{  
+  $arguments = "& '" +$myinvocation.mycommand.definition + "'"
+  Start-Process powershell -Verb runAs -ArgumentList $arguments
+  Break
+}
 #----------------------------------------------------------[Initialization & Declarations]----------------------------------------------------------
 
 #Set Error Action to Silently Continue
