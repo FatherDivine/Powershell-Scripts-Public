8eefc4f04fa5519ecd1008923c07ee6cd5bed45d
FatherDivine <150489354+FatherDivine@users.noreply.github.com>
2023-11-22 11:38:08 -0700

    Delete GPO Scripts directory

--- GPO Scripts/HRtest2.bat
+++
@@ -1,3 +0,0 @@
-@echo off
-powershell -File \\ucdenver.pvt\SysVol\ucdenver.pvt\Policies\{5A42BC16-A675-4A79-895D-D830B56C6E41}\User\Scripts\Logon\HardwareReadiness.ps1 >> \\ucdenver.pvt\SysVol\ucdenver.pvt\Policies\{5A42BC16-A675-4A79-895D-D830B56C6E41}\User\Scripts\Logon\W11Data
-
