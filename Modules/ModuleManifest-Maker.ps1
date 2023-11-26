b94aab7b8832b7cf61da01d0581b8fb07183ea5b
FatherDivine <aaron.staten@ucdenver.edu>
2023-11-26 04:12:16 -0700

    Build/Manifest files

--- Modules/ModuleManifest-Maker.ps1
+++ Modules/ModuleManifest-Maker.ps1
@@ -1,3 +1,5 @@
 $guid2 = [guid]::NewGuid()
 
 New-ModuleManifest -path "C:\Program Files\WindowsPowerShell\Modules\Logging-dirFunctions\Logging-Functions.psd1" -Guid $guid2 -Author 'Aaron Staten' -Description 'Various logging functions used by the CEDC PS Script Template' -ModuleVersion 0.1
+
+#invoke-psake as well in the build folder (syntax = verb-noun.ps1)
