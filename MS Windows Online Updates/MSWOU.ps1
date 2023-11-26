67b0d3657fce84d5b3fa09ebdcfed89ffc5a4615
FatherDivine <aaron.staten@ucdenver.edu>
2023-11-25 17:51:37 -0700

    One Last Effort

--- MS Windows Online Updates/MSWOU.ps1
+++ MS Windows Online Updates/MSWOU.ps1
@@ -146,7 +146,7 @@ Function MSWOnlineUpdater{
       Set-Item WSMan:\localhost\Client\TrustedHosts -Value "*.ucdenver.pvt" -Force -Verbose
 
       #Install updates on remote pc(s).
-      Invoke-WUInstall -ComputerName $ComputerName -Script {Import-Module PSWindowsUpdate; Install-WindowsUpdate -AcceptAll -AutoReboot -MicrosoftUpdate | Format-Table -AutoSize -Wrap | Out-File (New-Item -Path "C:\Windows\Logs\MSWOU\PSWindowsUpdate-List$using:date.log" -Force)} `
+      Invoke-WUInstall -ComputerName $ComputerName -Script {Import-Module PSWindowsUpdate; Install-WindowsUpdate -AcceptAll -AutoReboot -MicrosoftUpdate | Format-Table -AutoSize -Wrap | Out-File (New-Item -Path "C:\Windows\Logs\MSWOU\PSWindowsUpdate-List$date.log" -Force)} `
       -Confirm:$false -SkipModuleTest -RunNow -Verbose
 
       #Waits 60 minutes to check the status of the last 100 updates and logs to file
