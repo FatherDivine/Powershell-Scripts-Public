621e946d8ce0e71fd0f16adce2d20db7e0587be0
FatherDivine <aaron.staten@ucdenver.edu>
2023-11-29 00:07:41 -0700

    Deleting Redundant Files

--- Javier-SquidProxy/Redundant/Tester.ps1
+++
@@ -1,7 +0,0 @@
-﻿$Pcs = Get-Content "${PSScriptRoot}\LW840.txt"
-foreach ($pc in $pcs){
-Invoke-Command -ComputerName $pc -ScriptBlock{
-New-Item "HKLM:\Software\Policies\Microsoft\Windows\CurrentVersion\Internet Settings"
-New-ItemProperty -Path "HKLM:\Software\Policies\Microsoft\Windows\CurrentVersion\Internet Settings" -Name ProxySettingsPerUser -Value 0
-}
-}
