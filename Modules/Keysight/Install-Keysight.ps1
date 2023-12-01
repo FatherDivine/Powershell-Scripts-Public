f54f3b9346373af179756c50d98ba5216d2647d5
FatherDivine <aaron.staten@ucdenver.edu>
2023-12-01 07:09:18 -0700

    usage update

--- Modules/Keysight/Install-Keysight.ps1
+++ Modules/Keysight/Install-Keysight.ps1
@@ -45,10 +45,10 @@ $sScriptVersion = "1.0"
 
 #Script Execution goes here, when not using as a Module
 
-If (!(Test-Path "C:\Program Files\WindowsPowerShell\Modules\Keysight")){  
+#If (!(Test-Path "C:\Program Files\WindowsPowerShell\Modules\Keysight")){  
     Write-Verbose 'Downloading the latest Keysight module and placing in C:\Program Files\WindowsPowerShell\Modules\Keysight\' -Verbose
     Invoke-WebRequest -Uri "https://raw.githubusercontent.com/FatherDivine/Powershell-Scripts-Public/main/Modules/Keysight/Keysight.psm1" -OutFile (New-Item -Path 'C:\Program Files\WindowsPowerShell\Modules\Keysight\Keysight.psm1' -Force) -Verbose
     Invoke-WebRequest -Uri "https://raw.githubusercontent.com/FatherDivine/Powershell-Scripts-Public/main/Modules/Keysight/Keysight.psd1" -OutFile (New-Item -Path 'C:\Program Files\WindowsPowerShell\Modules\Keysight\Keysight.psd1' -Force) -Verbose
     Invoke-WebRequest -Uri "https://raw.githubusercontent.com/FatherDivine/Powershell-Scripts-Public/main/Modules/Keysight/README.md" -OutFile (New-Item -Path 'C:\Program Files\WindowsPowerShell\Modules\Keysight\README.md' -Force) -Verbose
-  }
+  #}
   Import-Module -Name Keysight -DisableNameChecking
