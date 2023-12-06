39af7a30516fd353fbceb304e63e2b19f743d3da
FatherDivine <aaron.staten@ucdenver.edu>
2023-12-05 21:34:36 -0700

    Module updates

--- Modules/Javier-SquidProxy/Disable-Proxy/Disable-Proxy.psm1
+++ Modules/Javier-SquidProxy/Disable-Proxy/Disable-Proxy.psm1
@@ -43,37 +43,54 @@ https://github.com/FatherDivine/Powershell-Scripts-Public/blob/main/Javier-Squid
 #Variable declaration
 $date = Get-Date -Format "MM-dd-yyyy-HH-mm"
 
-#Define HKU as it isn't there by default
-New-PSDrive -PSProvider Registry -Name HKU -Root HKEY_USERS
-
-#Define our registry keys
-$regKeys1 = @(
-    "HKCU:\Software\Microsoft\Windows\CurrentVersion\Internet Settings\Connections",
-    "HKLM:\Software\Microsoft\Windows\CurrentVersion\Internet Settings\Connections",
-    "HKU:\.DEFAULT\Software\Microsoft\Windows\CurrentVersion\Internet Settings\Connections"
-)
-$regKeys2 = @(
-    "HKCU:\Software\Microsoft\Windows\CurrentVersion\Internet Settings",
-    "HKLM:\Software\Microsoft\Windows\CurrentVersion\Internet Settings",
-    "HKU:\.DEFAULT\Software\Microsoft\Windows\CurrentVersion\Internet Settings"
-)
-$PreventProxyChanges = "HKCU:\SOFTWARE\Policies\Microsoft\Internet Explorer\Control Panel" 
 #-----------------------------------------------------------[Execution]------------------------------------------------------------
 Function Disable-Proxy{
-#Undo the lockdown on editing proxy configuration
-Remove-ItemProperty -path $PreventProxyChanges Proxy -Force -ErrorAction SilentlyContinue
+  [cmdletbinding()]
+  Param()
 
-#Remove the proxy keys
-$regKeys1 | ForEach-Object {
-Remove-ItemProperty -Path $_ SavedLegacySettings -Force -ErrorAction SilentlyContinue
-Remove-ItemProperty -Path $_ DefaultConnectionSettings -Force -ErrorAction SilentlyContinue
-}
-$regKeys2 | ForEach-Object {
-Remove-ItemProperty -Path $_ ProxyEnable -Force -ErrorAction SilentlyContinue
-Remove-ItemProperty -Path $_ ProxyServer -Force -ErrorAction SilentlyContinue
-Remove-ItemProperty -Path $_ ProxyOverride -Force -ErrorAction SilentlyContinue
-}
+  Begin{
+    #Define HKU as it isn't there by default
+    New-PSDrive -PSProvider Registry -Name HKU -Root HKEY_USERS
 
-#Log
-Write-Output "Proxy Disabled on $date" | Out-File (New-Item -Path "C:\Windows\Logs\Proxy\ProxyStatus.txt" -Force)
+    #Define our registry keys
+    $regKeys1 = @(
+      "HKCU:\Software\Microsoft\Windows\CurrentVersion\Internet Settings\Connections",
+      "HKLM:\Software\Microsoft\Windows\CurrentVersion\Internet Settings\Connections",
+      "HKU:\.DEFAULT\Software\Microsoft\Windows\CurrentVersion\Internet Settings\Connections"
+    )
+    $regKeys2 = @(
+      "HKCU:\Software\Microsoft\Windows\CurrentVersion\Internet Settings",
+      "HKLM:\Software\Microsoft\Windows\CurrentVersion\Internet Settings",
+      "HKU:\.DEFAULT\Software\Microsoft\Windows\CurrentVersion\Internet Settings"
+    )
+    $PreventProxyChanges = "HKCU:\SOFTWARE\Policies\Microsoft\Internet Explorer\Control Panel" 
+  }
+  
+  Process{
+    Try{
+      #Undo the lockdown on editing proxy configuration
+      Remove-ItemProperty -path $PreventProxyChanges Proxy -Force -ErrorAction SilentlyContinue
+
+      #Remove the proxy keys
+      $regKeys1 | ForEach-Object {
+      Remove-ItemProperty -Path $_ SavedLegacySettings -Force -ErrorAction SilentlyContinue
+      Remove-ItemProperty -Path $_ DefaultConnectionSettings -Force -ErrorAction SilentlyContinue
+      }
+      $regKeys2 | ForEach-Object {
+      Remove-ItemProperty -Path $_ ProxyEnable -Force -ErrorAction SilentlyContinue
+      Remove-ItemProperty -Path $_ ProxyServer -Force -ErrorAction SilentlyContinue
+      Remove-ItemProperty -Path $_ ProxyOverride -Force -ErrorAction SilentlyContinue
+      }
+
+      #Log
+      Write-Output "Proxy Disabled on $date" | Out-File (New-Item -Path "C:\Windows\Logs\Proxy\ProxyStatus.txt" -Force)
+      }
+    Catch{
+      Write-Verbose "Error Detected: $_.Exception" -Verbose
+      Break
+    }  
+  }
+  End{ 
+    If($?){}
+  }
 }
--- Modules/Javier-SquidProxy/Enable-Proxy/Enable-Proxy.psm1
+++ Modules/Javier-SquidProxy/Enable-Proxy/Enable-Proxy.psm1
@@ -37,32 +37,53 @@ https://github.com/FatherDivine/Powershell-Scripts-Public/blob/main/Javier-Squid
   If you want to be fancy and make each it's own job.
   This method is good for keeping track of which PC may have failed/offline.
 #>
+
 #----------------------------------------------------------[Initialization & Declarations]----------------------------------------------------------
 
 #Variable declaration
 $date = Get-Date -Format "MM-dd-yyyy-HH-mm"
 
-#Define HKU as it isn't there by default
-New-PSDrive -PSProvider Registry -Name HKU -Root HKEY_USERS
-
-#Define our registry keys
-$regKeys = @(
-    "HKCU:\Software\Microsoft\Windows\CurrentVersion\Internet Settings",
-    "HKLM:\Software\Microsoft\Windows\CurrentVersion\Internet Settings",
-    "HKU:\.DEFAULT\Software\Microsoft\Windows\CurrentVersion\Internet Settings"
-)
-$PreventProxyChanges = "HKCU:\SOFTWARE\Policies\Microsoft\Internet Explorer\Control Panel" 
 #-----------------------------------------------------------[Execution]------------------------------------------------------------
 Function Enable-Proxy{
-#Adding the below values to the above registry keys
-$regKeys | ForEach-Object {
-New-ItemProperty -path $_ ProxyEnable -value 1 -Force -ErrorAction SilentlyContinue
-New-ItemProperty -path $_ ProxyServer -value "dceasapp783:3128" -Force -ErrorAction SilentlyContinue
-New-ItemProperty -path $_ ProxyOverride -value "<local>" -Force -ErrorAction SilentlyContinue
-}
-#Lockdown the changes
-New-ItemProperty -path $PreventProxyChanges Proxy -value 1 -Force -ErrorAction SilentlyContinue
+  [cmdletbinding()]
+  Param()
+  
+  Begin{
+    #Define HKU as it isn't there by default
+    New-PSDrive -PSProvider Registry -Name HKU -Root HKEY_USERS
 
-#Log
-Write-Output "Proxy Enabled on $date" | Out-File (New-Item -Path "C:\Windows\Logs\Proxy\ProxyStatus.txt" -Force)
+    #Define our registry keys
+    $regKeys = @(
+      "HKCU:\Software\Microsoft\Windows\CurrentVersion\Internet Settings",
+      "HKLM:\Software\Microsoft\Windows\CurrentVersion\Internet Settings",
+      "HKU:\.DEFAULT\Software\Microsoft\Windows\CurrentVersion\Internet Settings"
+    )
+
+    $PreventProxyChanges = "HKCU:\SOFTWARE\Policies\Microsoft\Internet Explorer\Control Panel" 
+  }
+
+  Process{
+    Try{
+    #Adding the below values to the above registry keys
+    $regKeys | ForEach-Object {
+      New-ItemProperty -path $_ ProxyEnable -value 1 -Force -ErrorAction SilentlyContinue
+      New-ItemProperty -path $_ ProxyServer -value "dceasapp783:3128" -Force -ErrorAction SilentlyContinue
+      New-ItemProperty -path $_ ProxyOverride -value "<local>" -Force -ErrorAction SilentlyContinue
+    }
+    #Lockdown the changes
+    New-ItemProperty -path $PreventProxyChanges Proxy -value 1 -Force -ErrorAction SilentlyContinue
+
+    #Log
+    Write-Output "Proxy Enabled on $date" | Out-File (New-Item -Path "C:\Windows\Logs\Proxy\ProxyStatus.txt" -Force)
+    }
+
+    Catch{
+      Write-Verbose "Error Detected: $_.Exception" -Verbose
+      Break
+    }
+  }
+
+  end{
+    If($?){}
+  }
 }
