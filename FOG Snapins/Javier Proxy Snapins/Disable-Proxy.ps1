5e9f51b8bd451cc092ef42f590a647c6c49e57cf
FatherDivine <aaron.staten@ucdenver.edu>
2023-12-07 13:25:35 -0700

    Update FOG snap-in use script

--- FOG Snapins/Javier Proxy Snapins/Disable-Proxy.ps1
+++ FOG Snapins/Javier Proxy Snapins/Disable-Proxy.ps1
@@ -5,7 +5,7 @@
 .DESCRIPTION
   This script disables Javiar's Squid proxy for exams
   taken in the CEDC, specifically Computer Science.
-    
+
 .INPUTS
   None. You cannot pipe objects to Disable-Proxy.ps1.
 
@@ -13,22 +13,22 @@
   The Proxy Status (Enabled/Disabled) stored in C:\Windows\Logs\Proxy\
 
 .NOTES
-  Version:        0.1
+  Version:        0.2
   Author:         Aaron Staten
-  Creation Date:  11/28/2023
+  Creation Date:  11/28/2023 (Last updated 12/5/2023)
   Purpose:        For CEDC IT Dept. use
 
 .LINK
 https://github.com/FatherDivine/Powershell-Scripts-Public/blob/main/Javier-SquidProxy/Disable-Proxy.ps1
-  
+
 .EXAMPLE
   & .\Enable-Proxy.ps1
 
   The simplest execution from a PowerShell prompt.
 
 .EXAMPLE
-  Invoke-Command -FilePath .\Enable-Proxy.ps1 -ComputerName $PCs 
-  
+  Invoke-Command -FilePath .\Enable-Proxy.ps1 -ComputerName $PCs
+
   To invoke, thereby sending the script to a single PC or array of PCs.
 
 .EXAMPLE
@@ -43,36 +43,60 @@ https://github.com/FatherDivine/Powershell-Scripts-Public/blob/main/Javier-Squid
 #Variable declaration
 $date = Get-Date -Format "MM-dd-yyyy-HH-mm"
 
-#Define HKU as it isn't there by default
-New-PSDrive -PSProvider Registry -Name HKU -Root HKEY_USERS
+#-----------------------------------------------------------[Functions]------------------------------------------------------------
 
-#Define our registry keys
-$regKeys1 = @(
-    "HKCU:\Software\Microsoft\Windows\CurrentVersion\Internet Settings\Connections",
-    "HKLM:\Software\Microsoft\Windows\CurrentVersion\Internet Settings\Connections",
-    "HKU:\.DEFAULT\Software\Microsoft\Windows\CurrentVersion\Internet Settings\Connections"
-)
-$regKeys2 = @(
-    "HKCU:\Software\Microsoft\Windows\CurrentVersion\Internet Settings",
-    "HKLM:\Software\Microsoft\Windows\CurrentVersion\Internet Settings",
-    "HKU:\.DEFAULT\Software\Microsoft\Windows\CurrentVersion\Internet Settings"
-)
-$PreventProxyChanges = "HKCU:\SOFTWARE\Policies\Microsoft\Internet Explorer\Control Panel" 
+Function Disable-Proxy{
+  [cmdletbinding()]
+  Param()
+
+  Begin{
+    #Define HKU as it isn't there by default
+    New-PSDrive -PSProvider Registry -Name HKU -Root HKEY_USERS
+
+    #Define our registry keys
+    $regKeys1 = @(
+      "HKCU:\Software\Microsoft\Windows\CurrentVersion\Internet Settings\Connections",
+      "HKLM:\Software\Microsoft\Windows\CurrentVersion\Internet Settings\Connections",
+      "HKU:\.DEFAULT\Software\Microsoft\Windows\CurrentVersion\Internet Settings\Connections"
+    )
+    $regKeys2 = @(
+      "HKCU:\Software\Microsoft\Windows\CurrentVersion\Internet Settings",
+      "HKLM:\Software\Microsoft\Windows\CurrentVersion\Internet Settings",
+      "HKU:\.DEFAULT\Software\Microsoft\Windows\CurrentVersion\Internet Settings"
+    )
+    $PreventProxyChanges = "HKCU:\SOFTWARE\Policies\Microsoft\Internet Explorer\Control Panel"
+  }
+
+  Process{
+    Try{
+      #Undo the lockdown on editing proxy configuration
+      Remove-ItemProperty -path $PreventProxyChanges Proxy -Force -ErrorAction SilentlyContinue
+
+      #Remove the proxy keys
+      $regKeys1 | ForEach-Object {
+      Remove-ItemProperty -Path $_ SavedLegacySettings -Force -ErrorAction SilentlyContinue
+      Remove-ItemProperty -Path $_ DefaultConnectionSettings -Force -ErrorAction SilentlyContinue
+      }
+      $regKeys2 | ForEach-Object {
+      Remove-ItemProperty -Path $_ ProxyEnable -Force -ErrorAction SilentlyContinue
+      Remove-ItemProperty -Path $_ ProxyServer -Force -ErrorAction SilentlyContinue
+      Remove-ItemProperty -Path $_ ProxyOverride -Force -ErrorAction SilentlyContinue
+      }
+
+      #Log
+      Write-Output "Proxy Disabled on $date" | Out-File (New-Item -Path "C:\Windows\Logs\Proxy\ProxyStatus.txt" -Force)
+      }
+    Catch{
+      Write-Verbose "Error Detected: $_.Exception" -Verbose
+      Break
+    }
+  }
+  End{
+    If($?){}
+  }
+}
+
 #-----------------------------------------------------------[Execution]------------------------------------------------------------
-$reglog= @()
-#Undo the lockdown on editing proxy configuration
-Remove-ItemProperty -path $PreventProxyChanges Proxy -Force -ErrorAction SilentlyContinue
-
-#Remove the proxy keys
-$regKeys1 | ForEach-Object {
-Remove-ItemProperty -Path $_ SavedLegacySettings -Force -ErrorAction SilentlyContinue
-Remove-ItemProperty -Path $_ DefaultConnectionSettings -Force -ErrorAction SilentlyContinue
-}
-$regKeys2 | ForEach-Object {
-Remove-ItemProperty -Path $_ ProxyEnable -Force -ErrorAction SilentlyContinue
-Remove-ItemProperty -Path $_ ProxyServer -Force -ErrorAction SilentlyContinue
-Remove-ItemProperty -Path $_ ProxyOverride -Force -ErrorAction SilentlyContinue
-}
-
-#Log
-Write-Output "$reglog`nProxy Disabled on $date" | Out-File (New-Item -Path "C:\Windows\Logs\Proxy\ProxyStatus.txt" -Force)
+#Execution for FOG Snap-in purposes
+& Disable-Proxy
+exit
